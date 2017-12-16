f717cb11732f566f1120d12d777b768a1278a1c6
Emir Unluturk <cenonx@gmail.com>
2017-12-17 01:06:13 +0300

    Implement Try/Catch

--- src/PassMan/AESGenerator.java
+++ src/PassMan/AESGenerator.java
@@ -1,7 +1,7 @@
 package PassMan;
 
+import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
-import java.security.spec.InvalidKeySpecException;
 import javax.crypto.*;
 import java.security.SecureRandom;
 import java.util.Base64;
@@ -9,38 +9,51 @@ import java.util.Base64;
 public class AESGenerator {
 	private SecretKey key;
 	private static final String ALGORITHM = "AES";
-	
-	public AESGenerator() {
-		/*try {
 
-		} catch (InvalidKeySpecException | NoSuchAlgorithmException | NoSuchPaddingException e) {
-			throw new RuntimeException("oops", e);
-		}*/
-		
+	public AESGenerator() {
+		/*
+		 * try {
+		 * 
+		 * } catch (InvalidKeySpecException | NoSuchAlgorithmException |
+		 * NoSuchPaddingException e) { throw new RuntimeException("oops", e); }
+		 */
+
 		// might add nopadding later on so ill keep try catch for now
 	}
-	
+
 	public void initRandom() {
-		SecureRandom random = SecureRandom.getInstanceStrong();
-       	  	KeyGenerator keyGen = KeyGenerator.getInstance(ALGORITHM);
-        	keyGen.init(128, random);
-		key = keyGen.generateKey();
+		try {
+			SecureRandom random = SecureRandom.getInstanceStrong();
+			KeyGenerator keyGen = KeyGenerator.getInstance(ALGORITHM);
+			keyGen.init(128, random);
+			key = keyGen.generateKey();
+		} catch (NoSuchAlgorithmException e) {
+			throw new RuntimeException("NoAlgorithm Oops!", e);
+		}
 	}
 
 	public String encrypt(String pass) {
-		initRandom();
-		Cipher c = Cipher.getInstance(ALGORITHM);
-        	c.init(Cipher.ENCRYPT_MODE, key);
-        	byte[] encVal = c.doFinal(pass.getBytes());
-        	return Base64.getEncoder().encodeToString(encVal);
+		try {
+			initRandom();
+			Cipher c = Cipher.getInstance(ALGORITHM);
+			c.init(Cipher.ENCRYPT_MODE, key);
+			byte[] encVal = c.doFinal(pass.getBytes());
+			return Base64.getEncoder().encodeToString(encVal);
+		} catch (NoSuchAlgorithmException | InvalidKeyException | NoSuchPaddingException | IllegalBlockSizeException | BadPaddingException e) {
+			throw new RuntimeException("Encryption Error Oops!", e);
+		}
 	}
 
 	public String decrypt(String encryptedPass) {
-		initRandom();
-		Cipher c = Cipher.getInstance(ALGORITHM);
-       	 	c.init(Cipher.DECRYPT_MODE, key);
-       	 	byte[] decodedValue = Base64.getDecoder().decode(encryptedPass);
-        	byte[] decValue = c.doFinal(decodedValue);
-        	return new String(decValue);
+		try {
+			initRandom();
+			Cipher c = Cipher.getInstance(ALGORITHM);
+			c.init(Cipher.DECRYPT_MODE, key);
+			byte[] decodedValue = Base64.getDecoder().decode(encryptedPass);
+			byte[] decValue = c.doFinal(decodedValue);
+			return new String(decValue);
+		} catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException | BadPaddingException e) {
+			throw new RuntimeException("Decryption Error Oops!", e);
+		}
 	}
 }
