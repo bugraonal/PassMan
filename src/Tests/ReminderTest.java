7f36d2be866652071de0051108b1917ff613205b
onatozu <onat.gungor@ozu.edu.tr>
2017-12-19 17:51:22 +0300

    Update ReminderTest.java

--- src/Tests/ReminderTest.java
+++ src/Tests/ReminderTest.java
@@ -14,5 +14,9 @@ public class ReminderTest {
 		
 		System.out.println(rem.dispDueDate());
 		System.out.println(rem.dispRemainingTime());
+		assertEquals(0, rem.setDueDate(0,"days"), "Amount should be greater than 0");
+        	assertEquals(0, rem.setDueDate(0,"months"), "Amount should be greater than 0");
+        	assertEquals(0, rem.setDueDate(0,"years"), "Amount should be greater than 0");
+
 	}
 }
