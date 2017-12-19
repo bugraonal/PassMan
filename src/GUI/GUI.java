41cc72e89b22ae0481ba503c1c46bf8a1d49d549
Emir Unluturk <cenonx@gmail.com>
2017-12-20 01:51:57 +0300

    Update LoginPanel

--- src/GUI/GUI.java
+++ src/GUI/GUI.java
@@ -25,13 +25,35 @@ public class GUI extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setTitle("PassMan");
 		setResizable(false);
-		setSize(450, 299);
+		//setSize(450, 299);
+		setDynamicSize();
+		setVisible(true);
 		add(contentPanel);
 	}
 
+	private void setDynamicSize() {
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		int width = (int) (screenSize.getWidth() / 4);
+		int height = (int) screenSize.getHeight() - getTaskBarHeight() - 300;
+		int yPos = 0;
+		int xPos = (int) (screenSize.getWidth() - width);
+		setBounds(xPos, yPos, width, height);
+	}
+	
+	private int getTaskBarHeight() {
+		int taskBarHeight = Toolkit.getDefaultToolkit().getScreenInsets(
+				getGraphicsConfiguration()).bottom;
+		return taskBarHeight;
+	}
+
 	public void printLoginPanel() {
-		contentPanel = new LoginPanel();
-		validate();
+		getContentPane().removeAll();
+		if (loginPanel == null)
+			loginPanel = new LoginPanel(this);
+		loginPanel.printLoginPanel();
+		add(loginPanel);
+		getContentPane().repaint();
+		getContentPane().validate();
 	}
 	
 	// I'm leaving GUI related stuff to Onur
