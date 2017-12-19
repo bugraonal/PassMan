5ecfe3eb3cb0580fae97e2b3304c02d3af4ea961
Emir Unluturk <cenonx@gmail.com>
2017-12-20 01:00:23 +0300

    Simple Password String Generator

---
+++ src/PassMan/PassGen.java
@@ -0,0 +1,20 @@
+package PassMan;
+
+import java.security.SecureRandom;
+
+public class PassGen {
+	static final String AB = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	static SecureRandom rnd = new SecureRandom();
+	
+	private PassGen() {
+		
+	}
+	
+	public String generatePassword() {
+		StringBuilder sb = new StringBuilder(12);
+		for( int i = 0; i < 12; i++ ) 
+		      sb.append( AB.charAt( rnd.nextInt(AB.length()) ) );
+		return sb.toString();
+	}
+
+}
